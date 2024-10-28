6153ee7cb68aab566455042acd2b2b410ee3daa8
Huntertk <taufik.khan65@gmail.com>
2024-10-29 00:42:25 +0530

    frontend setup sass

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { lazy, Suspense } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+const Layout = lazy(() => import('./components/Layout'))
+const Home = lazy(() => import('./pages/Home'))
 
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+    <BrowserRouter>
+      <Suspense fallback={<h1>Loading.....</h1>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />}/>
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
