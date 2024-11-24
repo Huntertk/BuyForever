3695a695e29b2c68f63e87502afe971ba59eea22
Huntertk <taufik.khan65@gmail.com>
2024-11-24 20:10:03 +0530

    global page loader created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { lazy, Suspense } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 const Layout = lazy(() => import('./components/Layout'))
+const Loader = lazy(() => import('./components/Loader'))
 const Home = lazy(() => import('./pages/Home'))
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<h1>Loading.....</h1>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />}/>
