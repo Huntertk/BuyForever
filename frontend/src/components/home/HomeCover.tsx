f1f8b1e611212c7c175caf9adfdac28f73f14a4d
Huntertk <taufik.khan65@gmail.com>
2024-11-25 16:03:48 +0530

    theme changed of website

--- frontend/src/components/home/HomeCover.tsx
+++ frontend/src/components/home/HomeCover.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import '../../styles/homeCover.scss';
 
 const HomeCover = () => {
+  const navigate = useNavigate()
   return (
     <div className="home_cover_main_container">
         <h1>Shaping a New</h1>
@@ -8,7 +10,7 @@ const HomeCover = () => {
         <h1>Sophistication</h1>
         <p>Elevate your wardrobe and embrace your </p>
         <p>Unique elegance with every click</p>
-        <button>Explore now</button>
+        <button onClick={() => navigate("/collection")}>Explore now</button>
     </div>
   )
 }
